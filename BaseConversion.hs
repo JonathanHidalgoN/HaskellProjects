8a49efcdaae9746e5a32864946adb14e3c9891a3
Jonathan <joni7840850@gmail.com>
2023-07-04 16:12:28 -0600

    Add parity check

--- BaseConversion.hs
+++ BaseConversion.hs
@@ -1,7 +1,6 @@
 {-
 Program to do base conversion from numbers in base 2 to base 10 and vice-versa 
 -}
-
 import Data.Char
 
 type Bit = Int
@@ -33,6 +32,17 @@ make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
 {-
+Add 1 in the head if the number of ones
+is even
+-}
+addParity :: [Bit] -> [Bit]
+addParity xs 
+    | even nos = 1 : xs
+    | otherwise = 0 : xs
+    where 
+        nos =  length (filter (==1) xs)
+
+{-
 Conver a string to a sequence of bits
 We have a string that is a list of characters, we apply a
 map, so for each character we transform to ASCII with ord,
@@ -40,7 +50,7 @@ then it is an int, so we pass to binary, then make it 8 bits,
 then concat each list
 -}
 encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+encode = addParity. concat . map (make8 . int2bin . ord)
 
 {-
 Given a list of bits, separate it into lists f 8 bits
@@ -50,12 +60,23 @@ chop8 [] = []
 chop8 bits = take 8 bits : chop8 (drop 8 bits)
 
 {-
+Check parity in messages
+-}
+checkParity :: [Bit] -> [Bit]
+checkParity xs 
+    | even nos && head xs == 1 = tail xs
+    | odd nos && head xs == 0 = tail xs 
+    | otherwise = error "Corrputed"
+    where 
+        nos = length . filter (== 1) . tail $ xs
+
+{-
 Given a list of bits, we take each 8 bits(1 character),
 pass to an int and to a character, then do it in the whole list of
 bits
 -}
 decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+decode = map (chr . bin2int) . chop8 . checkParity
 
 {-
 Just the identity function
